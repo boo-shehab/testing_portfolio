672e958ec9a6e59f8e83d361a833073d78eb85c2
Ahmed Abas Al-Ali <bfhetyrhyr12@gmail.com>
2025-10-30 12:34:56 +0300

    gh-pages

--- frontend_react/vite.config.js
+++ frontend_react/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/testing_portfolio'
   esbuild: {
     jsxInject: `import React from 'react'`,
   },
